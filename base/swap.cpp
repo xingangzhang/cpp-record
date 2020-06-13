1a0fe1d8b653e3aaa94e5d8ebdc0012d4882062b
lilei <tothis@qq.com>
2020-06-13 23:05:50 +0800

    交换变量

--- base/swap.cpp
+++ base/swap.cpp
@@ -22,16 +22,32 @@ void swap2(int &a, int &b) {
 }
 
 // 数值过大可能会导致内存溢出
-void swap3(int *a, int *b) {
-    *a = *a + *b;
-    *b = *a - *b;
-    *a = *a - *b;
+void swap3(int &a, int &b) {
+    /*
+        // a变量赋值为a + b
+        a = a + b;
+        // b变量赋值为a
+        b = a - b; // (a + b) - b = a
+        // a变量赋值为b
+        a = a - b; // 原理同b赋值
+    */
+    // 简写方式
+    b = a + b - (a = b);
 }
 
-void swap4(int *a, int *b) {
-    *a = *a ^ *b;
-    *b = *a ^ *b;
-    *a = *a ^ *b;
+void swap4(int &a, int &b) {
+    /*
+        // a变量赋值为a ^ b
+        a = a ^ b;
+        // b变量赋值为a
+        b = a ^ b; // (a ^ b) ^ b = a ^ (b ^ b) = a ^ 0 = a
+        // b变量赋值为a
+        a = a ^ b; // 原理同b赋值
+    */
+    // 简写方式1
+    // b = a ^ b ^ (a = b);
+    // 简写方式2
+    a ^= b ^= a ^= b;
 }
 
 /*
@@ -46,11 +62,11 @@ int main() {
     print(a, b);
 
     // 加减交换
-    swap3(&a, &b);
+    swap3(a, b);
     print(a, b);
 
     // 异或交换
-    swap4(&a, &b);
+    swap4(a, b);
     print(a, b);
 
     // c++库交换
