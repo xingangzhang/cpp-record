b095397ffc40f2bc3ac6548e9f0a271befd37c04
李磊 <this.lilei@gmail.com>
2020-12-28 17:18:07 +0800

    feat: 增加静态动态编译库文件记录

--- lib/test-lib.h
+++ lib/test-lib.h
@@ -5,7 +5,15 @@
 /**
  * extern "C" 使用C函数命名规则
  *  __declspec(dllexport) 导出函数
+ * Linux不需要声明导出函数
+ *
+ * Windows编译为DLL 编译test-lib-shared
+ * Linux编译为SO g++ test-lib.cpp -fPIC -shared -o liblibtest-lib-shared.so
  */
-extern "C" __declspec(dllexport)  int test_add(int a, int b);
+#ifdef Win32
+extern "C" __declspec(dllexport) int test_add(int a, int b);
+#else
+extern "C" int test_add(int a, int b);
+#endif
 
 #endif
